abcc9ccf0ee56ca630f25403a7440bac36c672d7
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-19 17:55:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,20 @@
 #def reverse_each_word(string)
+#  new_array = string.split(" ")
+#  reversed_array = []
+#  new_array.each do |element|
+#    reversed_array << element.reverse 
+#  end
+#  reversed_array.join(" ")
+#end
+
+#puts reverse_each_word("Hello there, and how are you?")
+
+def reverse_each_word(string)
   new_array = string.split(" ")
-  reversed_array = []
-  new_array.each do |element|
-    reversed_array << element.reverse 
+  new_array.collect do |element|
+    element.reverse
   end
-  reversed_array.join(" ")
+  new_array.join(" ")
+  
   
 end
-puts reverse_each_word("Hello there, and how are you?")
