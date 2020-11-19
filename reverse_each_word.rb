303600bc1c1ef149a305929d96816ccacf2e01e6
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-19 17:37:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   new_array = string.split(" ")
   
-  
+  p new_array
   
   
 end
-puts new_array("hello there, and how are you?")
+puts reverse_each_word("hello there, and how are you?")
